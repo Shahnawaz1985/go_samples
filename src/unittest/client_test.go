d6007e76b4c7fcc3f590325f5d24340a167867fb
mohammadshahnawazakhter <shahnawazakhter@live.com>
2021-11-20 16:59:07 +0530

    code formatted with util function and in Behvior Driven Design context.

--- src/unittest/client_test.go
+++ src/unittest/client_test.go
@@ -1,19 +1,32 @@
 package unittest
 
 import (
+	"fmt"
 	"testing"
 )
 
 func TestTwoElements(t *testing.T) {
 	list := []string{"apple", "orange"}
-	if JoinWithCommas(list) != "apple and orange" {
-		t.Errorf("didn't match expected values")
+	want := "apple and orange"
+	got := JoinWithCommas(list)
+	if got != want {
+		t.Errorf(errorString(list, got, want))
+	} else {
+		fmt.Println(errorString(list, got, want))
 	}
 }
 
 func TestThreeElements(t *testing.T) {
 	list := []string{"apple", "orange", "watermelon"}
-	if JoinWithCommas(list) != "apple, orange, and watermelon" {
-		t.Errorf("didn't match expected values")
+	want := "apple, orange, and watermelon"
+	got := JoinWithCommas(list)
+	if got != want {
+		t.Errorf(errorString(list, got, want))
+	} else {
+		fmt.Println(errorString(list, got, want))
 	}
+}
+
+func errorString(list []string, got string, want string) string {
+	return fmt.Sprintf("JoinWithCommas (%#v) = \"%s\", want \"%s\"", list, got, want)
 }
